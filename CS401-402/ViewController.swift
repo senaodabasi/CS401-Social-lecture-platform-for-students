e1ac30ad10fc8b84e3a22850dcc3e93623e1f93f
Adem Tunay Saka <atunaysaka@gmail.com>
2017-10-22 18:05:40 +0300

    Initial Commit

---
+++ CS401-402/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CS401-402
+//
+//  Created by Adem Tunay Saka on 22/10/2017.
+//  Copyright © 2017 atunaysaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
